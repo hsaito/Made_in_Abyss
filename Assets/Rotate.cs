fbdf5e2c4e7df1bf29ba0135382a5fea47c73e28
Hideki Saito <hideki@hidekisaito.com>
2020-01-30 22:55:06 -0800

    Initial commit

---
+++ Assets/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private GameObject _logo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _logo = GameObject.Find("MadeInAbyss");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _logo.transform.Rotate(0, 0, 0.2f, Space.Self);
+    }
+}
